c76deea9a2f553cec54e425f39d41c1eaf1639f4
Adrian Lachowicz <adrianlachowicz36@gmail.com>
2022-12-15 10:45:14 +0100

    Added a shell script to prepare data.
    Added a shell script to prepare data. The script makes appropriate directories, downloads and extracts data from .rar file to the specific directory

---
+++ bin/prepare_data.sh
@@ -0,0 +1,6 @@
+mkdir -p ./data
+mkdir -p ./data/raw
+mkdir -p ./data/external/resisc
+
+wget -O ./data/raw/resisc.rar https://sov8mq.dm.files.1drv.com/y4m4JOwqH0JXOhy8Y8QgSbFce3DlJKhuMVclXqqhqAPeUMnn1oEZ1O3seAcXqhzrrwFny_Oo3NuyLN_baLTq-eVEyhipspKOBrUrPIv3qr0DZTPvrq4SYCFInkim-_j4wa3IB6RKdSPPJ8Dhzyo8ZDRYD2xMpVVnJnhnqoEG8eG9fcBag1-dvcl9GkCF4XSlDnneyxiPNVH2uDcwCo0H_n2zA
+unrar x ./data/raw/resisc.rar ./data/external/resisc
