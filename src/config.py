84d9cb715c2a90f2bddada78d6d220f413d8ec38
Adrian Lachowicz <adrianlachowicz36@gmail.com>
2022-12-15 19:04:51 +0100

    Added specific configs to config file
    Added feature extractor config, classifier config, img config and general config to the config file.

--- src/config.py
+++ src/config.py
@@ -1,3 +1,25 @@
+import torch.cuda
+
 NUM_WORKERS = 1
 BATCH_SIZE = 32
 TRAIN_SIZE = 0.8
+
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+FEATURE_EXTRACTOR_CONFIG = {
+    "type": "pretrained",
+    "model_name": "mobilenetv2_100",
+    "pretrained": True,
+}
+
+CLASSIFIER_CONFIG = {
+    "type": "linear",
+    "input_dim": None,
+    "hidden_dims": [],
+    "output_dim": 45,
+    "activation_function_name": "ReLU",
+}
+
+IMG_CONFIG = {"channels": 3, "width": 256, "height": 256}
+
+GENERAL_CONFIG = {"optimizer": "Adam", "learning_rate": 0.001}
